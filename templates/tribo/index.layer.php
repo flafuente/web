755b5b3a7837ac288b01bf39e77ccc508e673047
Jorge Martín Martí <tcbpepo@gmail.com>
2014-04-03 22:04:42 +0200

    Puesta en marcha!
    quitado de titulos de prueba!

--- templates/tribo/index.layer.php
+++ templates/tribo/index.layer.php
@@ -35,7 +35,6 @@
 		<link rel="shortcut icon" href="<?=Url::template("img/favicon.png")?>">
 	</head>
 	<body class="">
-		hola tetas!
 		<!-- Module publiTop -->
 		<?=$controller->view("modules.publiTop");?>
 		<!--/Module publiTop-->		
