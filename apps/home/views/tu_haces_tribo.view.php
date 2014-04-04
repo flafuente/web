cfb1ef0923241ff528558d33023599871cc3015b
Jorge Martín Martí <tcbpepo@gmail.com>
2014-04-04 18:43:11 +0200

    Tu Haces Tribo + Imagenes Propias

--- apps/home/views/tu_haces_tribo.view.php
+++ apps/home/views/tu_haces_tribo.view.php
@@ -1,22 +1,48 @@
 <?php defined('_EXE') or die('Restricted access'); ?>
+<!-- Sub cabecera -->
 <div class="tu_haces">
 	<img class="img_tuhaces" src="<?=Url::template("/img/tu_haces.png");?>" alt="Tu haces tribo" title="Tú haces Tribo" />
 </div>
+
 <!-- tribo Cortos, noticias... -->
+
 <div class="col-md-2 c_green th_square">
+    <!--
     <h1><span>tribo</span> en Corto</h1>
+    -->
+    <div class="descr"></div>
 </div>
 <div class="col-md-2 c_yellow th_square">
+	<!--
     <h1><span>tribo</span> Notícias</h1>
+    -->
+    <div class="descr"></div>
 </div>
 <div class="col-md-2 c_red th_square">
+	<!--
     <h1><span>tribo</span> Música</h1>
+    -->
+    <div class="descr"></div>
 </div>
 <div class="col-md-2 c_pink th_square">
+	<!--
     <h1><span>tribo</span> Juegos</h1>
+    -->
+    <div class="descr"></div>
 </div>
 <div class="col-md-2 c_purple th_square">
+    <!--
     <h1><span>tribo</span></h1>
     <p>Tus Fotos</p>
     <p>Tus Videos</p>
+    -->
+    <div class="descr"></div>
+</div>
+<div style="clear: both;"></div>
+
+<!-- LOGIN -->
+<div class="col-md-10 th_entra">
+	<input type="text" name="usern" />
+	<input type="password" name="pwd" />
+	<input type="submit" />
 </div>
