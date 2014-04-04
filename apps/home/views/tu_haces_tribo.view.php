12c8f8f99810361f5c4e95e1fa8d3a16b0ba0e80
Jorge Martín Martí <tcbpepo@gmail.com>
2014-04-04 18:21:13 +0200

    CSS + Tu Haces Trivo (vers 1) + Fuentes
    Sin terminar "tu haces tribo"

---
+++ apps/home/views/tu_haces_tribo.view.php
@@ -0,0 +1,22 @@
+<?php defined('_EXE') or die('Restricted access'); ?>
+<div class="tu_haces">
+	<img class="img_tuhaces" src="<?=Url::template("/img/tu_haces.png");?>" alt="Tu haces tribo" title="Tú haces Tribo" />
+</div>
+<!-- tribo Cortos, noticias... -->
+<div class="col-md-2 c_green th_square">
+    <h1><span>tribo</span> en Corto</h1>
+</div>
+<div class="col-md-2 c_yellow th_square">
+    <h1><span>tribo</span> Notícias</h1>
+</div>
+<div class="col-md-2 c_red th_square">
+    <h1><span>tribo</span> Música</h1>
+</div>
+<div class="col-md-2 c_pink th_square">
+    <h1><span>tribo</span> Juegos</h1>
+</div>
+<div class="col-md-2 c_purple th_square">
+    <h1><span>tribo</span></h1>
+    <p>Tus Fotos</p>
+    <p>Tus Videos</p>
+</div>
