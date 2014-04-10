5b1081e5d106848e6320ceb7f5a5641999be48d1
Fernando Lafuente <soporte@spmedia.es>
2014-04-10 10:57:54 +0200

    Extras

--- templates/tribo/modules/publiLat.view.php
+++ templates/tribo/modules/publiLat.view.php
@@ -1,5 +1,5 @@
 <?php defined('_EXE') or die('Restricted access'); ?>
 
 <div class="publilat col-md-2 hidden-xs">
-    Publicidad
+    <img src='<?=Url::template("/img/publi.gif");?>' title='Herramientas' />
 </div>
