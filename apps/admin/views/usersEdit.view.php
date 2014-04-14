00d30e5f8704e1a075340345ee2b28beda8feb7f
gerard.arall@gmail.com <gerard.arall@gmail.com>
2014-04-14 21:11:08 +0200

    Perfil 1/2

--- apps/admin/views/usersEdit.view.php
+++ apps/admin/views/usersEdit.view.php
@@ -130,6 +130,69 @@ echo $controller->view("modules.toolbar");
 							<input type="password" id="password" name="password" class="form-control">
 						</div>
 					</div>
+					<!-- Username -->
+					<div class="form-group">
+						<label class="col-sm-2 control-label">
+							Alias
+						</label>
+						<div class="col-sm-10">
+							<input type="text" id="username" name="username" class="form-control" value="<?=Helper::sanitize($user->username);?>">
+						</div>
+					</div>
+					<!-- Sexo -->
+					<div class="form-group">
+						<label class="col-sm-2 control-label">
+							Sexo
+						</label>
+						<div class="col-sm-10">
+							<?php $s = array();?>
+							<?php $s[$user->sexo]; ?>
+							<input type="radio" goup="sexo" name="sexo" id="sexo1" value="1" <?=$s[1]?>>
+							Femenino
+							<input type="radio" goup="sexo" name="sexo" id="sexo2" value="2" <?=$s[1]?>>
+							Masculino
+						</div>
+					</div>
+					<!-- Cumpleaños -->
+					<div class="form-group">
+						<label class="col-sm-2 control-label">
+							Cumpleaños
+						</label>
+						<div class="col-sm-10">
+							<input type="date" id="fechaNacimiento" name="fechaNacimiento" class="form-control" value="<?=date("m/d/Y", strtotime($user->fechaNacimiento));?>">
+						</div>
+					</div>
+					<!-- Ubicacion -->
+					<div class="form-group">
+						<label class="col-sm-2 control-label">
+							Ubicacion
+						</label>
+						<div class="col-sm-10">
+							<input type="text" id="ubicacion" name="ubicacion" class="form-control" value="<?=Helper::sanitize($user->ubicacion);?>">
+						</div>
+					</div>
+					<!-- Biografía -->
+					<div class="form-group">
+						<label class="col-sm-2 control-label">
+							Biografía
+						</label>
+						<div class="col-sm-10">
+							<textarea id="biografia" name="biografia" class="form-control"><?=Helper::sanitize($user->biografia);?></textarea>
+							<br>
+							<input type="text" id="intereses" name="intereses" class="form-control" value="<?=Helper::sanitize($user->intereses);?>">
+						</div>
+					</div>
+					<!-- Formación y empleo -->
+					<div class="form-group">
+						<label class="col-sm-2 control-label">
+							Formación y empleo
+						</label>
+						<div class="col-sm-10">
+							<input type="text" id="trabajo" name="trabajo" class="form-control" value="<?=Helper::sanitize($user->trabajo);?>">
+							<br>
+							<input type="text" id="estudios" name="estudios" class="form-control" value="<?=Helper::sanitize($user->estudios);?>">
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
--- apps/perfil/views/edit.view.php
+++ apps/perfil/views/edit.view.php
@@ -13,44 +13,78 @@
 		<input type="hidden" name="action" id="action" value="save">
 		<div class="row">
 			<div class="col-md-12">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						Cuenta
+				<!-- Username -->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+						Alias
+					</label>
+					<div class="col-sm-10">
+						<input type="text" id="username" name="username" class="form-control" value="<?=Helper::sanitize($user->username);?>">
 					</div>
-				  	<div class="panel-body">
-						<!-- Email -->
-						<div class="form-group">
-							<label class="col-sm-2 control-label">
-								Email
-							</label>
-							<div class="col-sm-10">
-								<input type="text" id="email" name="email" class="form-control" value="<?=Helper::sanitize($user->email);?>">
-							</div>
-						</div>
-						<!-- Password -->
-						<div class="form-group">
-							<label class="col-sm-2 control-label">
-								Contraseña
-							</label>
-							<div class="col-sm-10">
-								<input type="password" id="password" name="password" class="form-control">
-							</div>
-						</div>
-						<!-- Buttons -->
-						<div class="form-group">
-							<div class="col-sm-offset-2 col-sm-10">
-								<a class="btn btn-default ladda-button" data-spinner-color="#000" data-style="slide-left" href="<?=Url::site();?>">
-									<span class="ladda-label">
-										Cancelar
-									</span>
-								</a>
-								<button class="btn btn-primary ladda-button" data-style="slide-left">
-									<span class="ladda-label">
-										Guardar
-									</span>
-								</button>
-							</div>
-						</div>
+				</div>
+				<!-- Sexo -->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+						Sexo
+					</label>
+					<div class="col-sm-10">
+						<?php $s = array();?>
+						<?php $s[$user->sexo]; ?>
+						<input type="radio" goup="sexo" name="sexo" id="sexo1" value="1" <?=$s[1]?>>
+						Femenino
+						<input type="radio" goup="sexo" name="sexo" id="sexo2" value="2" <?=$s[1]?>>
+						Masculino
+					</div>
+				</div>
+				<!-- Cumpleaños -->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+						Cumpleaños
+					</label>
+					<div class="col-sm-10">
+						<input type="date" id="fechaNacimiento" name="fechaNacimiento" class="form-control" value="<?=date("m/d/Y", strtotime($user->fechaNacimiento));?>">
+					</div>
+				</div>
+				<!-- Ubicacion -->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+						Ubicacion
+					</label>
+					<div class="col-sm-10">
+						<input type="text" id="ubicacion" name="ubicacion" class="form-control" value="<?=Helper::sanitize($user->ubicacion);?>">
+					</div>
+				</div>
+				<!-- Biografía -->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+						Biografía
+					</label>
+					<div class="col-sm-10">
+						<textarea id="biografia" name="biografia" class="form-control" placeholder="Cuéntanos algo sobre tí"><?=Helper::sanitize($user->biografia);?></textarea>
+						<br>
+						<input type="text" id="intereses" name="intereses" class="form-control" placeholder="intereses" value="<?=Helper::sanitize($user->intereses);?>">
+					</div>
+				</div>
+				<!-- Formación y empleo -->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+						Formación y empleo
+					</label>
+					<div class="col-sm-10">
+						<input type="text" id="trabajo" name="trabajo" class="form-control" placeholder="¿Dónde has trabajado?" value="<?=Helper::sanitize($user->trabajo);?>">
+						<br>
+						<input type="text" id="estudios" name="estudios" class="form-control" placeholder="¿Dónde has estudiado?" value="<?=Helper::sanitize($user->estudios);?>">
+					</div>
+				</div>
+
+				<!-- Buttons -->
+				<div class="form-group">
+					<div class="col-sm-offset-2 col-sm-10">
+						<button class="btn btn-primary ladda-button" data-style="slide-left">
+							<span class="ladda-label">
+								Guardar cambios
+							</span>
+						</button>
 					</div>
 				</div>
 			</div>
