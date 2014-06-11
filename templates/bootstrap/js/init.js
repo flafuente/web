dc7dce4af21afab29dbdddcd107816a94bc053bc
gerard.arall@gmail.com <gerard.arall@gmail.com>
2014-06-11 19:46:33 +0200

    Big update

---
+++ templates/bootstrap/js/init.js
@@ -0,0 +1,152 @@
+//Ajax forms
+$(document).on('submit', '.ajax', function(e){
+ 	$(".help-block").remove();
+	$(".alert").remove();
+	$(".has-warning").removeClass("has-warning");
+	$(".has-success").removeClass("has-success");
+	$(".has-error").removeClass("has-error");
+	var form = $(this);
+	var redirect = false;
+	$(this).ajaxSubmit({
+        dataType:  'json',
+		success:   function(data) {
+			//Messages
+			if(data.messages){
+				messages = data.messages;
+				if(messages.length){
+					for(var x=0;x<messages.length;x++) {
+						//Field message
+						if(messages[x].field){
+							if(messages[x].type=="danger"){
+								messages[x].type = "error";
+							}
+							field = form.find("select[name=" + messages[x].field + "], input[name=" + messages[x].field + "], textarea[name=" + messages[x].field + "], checkbox[name=" + messages[x].field + "]");
+							if(field.length){
+								field.parent().parent().addClass("has-" + messages[x].type);
+								field.parent().append('<span class="help-block">' + messages[x].message + '</span>');
+							}
+						//Url redirection
+						}else if(messages[x].url){
+							$(".alert").remove();
+							redirect = true;
+							document.location.href = messages[x].url;
+						//Message without field
+						}else{
+							if(messages[x].type=="error"){
+								messages[x].type = "danger";
+							}
+							$("#mensajes-sys").append('<div class="alert alert-' + messages[x].type + '"><button type="button" class="close" data-dismiss="alert">&times;</button>' + messages[x].message + '</div>');
+							$('html,body').animate({ scrollTop: 0 }, 'slow');
+						}
+					}
+				}
+			}
+			//Debug Message
+			if(data.debug){
+				if(data.debug.length){
+					messages = data.debug;
+					for(var x=0;x<messages.length;x++) {
+						//Increment counter
+						var total = parseInt($("#debugCounterMessagesAjax").html());
+						total++;
+						$("#debugCounterMessagesAjax").html(total);
+						//UL exists?
+						if(!$("#debugModalMessagesAjax ul.list-group").length){
+							//Create UL
+							$("#debugModalMessagesAjax .modal-body").append("<ul class='list-group'></ul>");
+							//Delete Blockquote
+							$("#debugModalMessagesAjax blockquote").remove();
+						}
+						//Add message
+						$("#debugModalMessagesAjax ul.list-group").append("<li class='list-group-item'><blockquote>" + messages[x].message + "</blockquote>" + messages[x].trace + "</li>");
+					}
+				}
+			}
+			//Extras
+			if(data.data){
+				//Modal HTML
+				if(data.data.modal){
+					$("#genericModal .modal-content").html(data.data.modal);
+					$("#genericModal").modal('show');
+				}
+			}
+			if(!redirect){
+				$(".btn.disabled").disabled = false;
+				$(".btn.disabled").removeClass("disabled");
+				//Lada spinners
+				Ladda.stopAll();
+			}
+		}
+	});
+	return false;
+});
+
+//Pagination
+$(document).on('click', '.pagination a', function(e){
+	var app = $(this).attr("data-app");
+	var action = $(this).attr("data-action");
+	var limit = $(this).attr("data-limit");
+	var limitStart = $(this).attr("data-limitStart");
+	var form = $(this).closest("form");
+	if(app){
+		checkFormField(form, "app", app);
+	}
+	if(action){
+		checkFormField(form, "action", action);
+	}
+	checkFormField(form, "limit", limit);
+	checkFormField(form, "limitStart", limitStart);
+	form.submit();
+});
+
+//Sortable Columns
+$(document).on('click', '.sortable', function(e){
+	var form = $(this).closest("form");
+	checkFormField(form, "order", $(this).attr("data-order"));
+	checkFormField(form, "orderDir", $(this).attr("data-orderDir"));
+	form.submit();
+});
+
+//Auto appends (if needed) hidden field
+function checkFormField(formElement, fieldName, fieldValue){
+	var field = formElement.find("input[name='" + fieldName + "']");
+	if(!field.length){
+		$('<input>').attr({
+		    type: 'hidden',
+		    name: fieldName,
+		    value: fieldValue
+		}).appendTo(formElement);
+	}else{
+		field.val(fieldValue);
+	}
+}
+
+//change-submit
+$(document).on('change', '.change-submit', function(e){
+	$('#mainForm').submit();
+});
+
+//delete buttons
+$(document).on('click', '.delete', function(e){
+	var res = false;
+	var confirmation = $(this).attr("confirm");
+	if(confirmation){
+		res = confirm(confirmation);
+	}else{
+		res = true;
+	}
+	if(res){
+		$('#action').val("delete");
+		$('#mainForm').submit();
+	}else{
+		Ladda.stopAll();
+	}
+	return false;
+});
+
+$(document).ready(function(){
+	//Bootsrap Switches
+	$("input[type='checkbox'].switch").bootstrapSwitch();
+	//Lada spinners
+	Ladda.bind('.ladda-button');
+});
---
+++ tests/seleniumConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+class seleniumConfig
+{
+
+    //Urls / Paths
+    public $folder = '/var/www/';
+    public $host = 'http://localhost';
+    public $path = '/LightFramework/';
+    //Screenshot
+    public $captureScreenshotOnFailure = true;
+    public $screenShotPath = 'tests/system/screenshots';
+    //Browser
+    public $browser = 'firefox';
+}
