4f58ac35814e89f4b1bbb6f104c09977445438d1
Jorge Martín Martí <tcbpepo@gmail.com>
2014-07-04 19:09:31 +0200

    Submenu + Adding Modals Search & Contact

--- templates/tribo/modules/mainMenu.view.php
+++ templates/tribo/modules/mainMenu.view.php
@@ -6,13 +6,38 @@
 		<?php $active = array(); ?>
 		<?php $active[$url->app][$url->action] = "active"; ?>
 
-	  <li class="<?=$active["programas"]["index"];?>"><a href="<?=Url::site("programas");?>">PROGRAMAS</a></li>
-	  <li class="<?=$active["informativos"]["index"];?>"><a href="<?=Url::site("informativos");?>">INFORMATIVOS</a></li>
-	  <li class="<?=$active["tvdirecto"]["index"];?>"><a href="<?=Url::site("tvdirecto");?>">TV EN DIRECTO</a></li>
+	  <li class="<?=$active["programas"]["index"];?> withsub">
+	  	<a href="<?=Url::site("programas");?>">PROGRAMAS</a>
+	  	<ul class="submenu" style="display: none;">
+	  		<div class="triangle"></div>
+	  		<?php
+	  		for($x=0; $x<12; $x++){
+	  			?><li class="col-md-6"><a href="<?=Url::site("programas/prog-".($x+1));?>"><?php echo "Elemento ".($x+1); ?></a></li><?php
+	  		}
+	  		?>
+	  		
+	  	</ul>
+	  </li>
+	  <li class="<?=$active["informativos"]["index"];?>">
+	  	<a href="<?=Url::site("informativos");?>">INFORMATIVOS</a>
+	  </li>
+	  <li class="<?=$active["tvdirecto"]["index"];?>">
+	  	<a href="<?=Url::site("tvdirecto");?>">TV EN DIRECTO</a>
+	  </li>
 	  <li class="<?=$active["haztetriber"]["index"];?>"><a href="<?=Url::site("haztetriber");?>">HAZTE TRIBER</a></li>
 	  <li class="<?=$active["quienessomos"]["index"];?>"><a href="<?=Url::site("quienessomos");?>">QUIÉNES SOMOS</a></li>
 	</ul>
 </div>
+
+<!-- Scripts Menu -->
+<script>
+	$(document).on("mouseover",".withsub",function(){
+        $(this).find('.submenu').css("display", "block");         
+    });
+    $(document).on("mouseout",".withsub",function(){
+        $(this).find('.submenu').css("display", "none");         
+    });
+</script>
 
 <!-- PARRILLA -->
 <div class='col-md-12' style='padding-left: 0px; padding-right: 0px;'>
@@ -39,4 +64,7 @@
 <!-- Sintonizanos -->
 <div class='col-md-12' style='padding-left: 0px; padding-right: 0px;'>
 	<a href="<?=Url::site("sintonizanos");?>" class="btn sintonizanos"><img src="<?=Url::template("img/weirdicon.png")?>" />&nbsp;&nbsp;SINTONÍZANOS</a>
+</div>
+<div class='col-md-12' style='padding-left: 0px; padding-right: 0px;'>
+	<a href="<?=Url::site("haztetriber");?>" class="btn sintonizanos betriber">be triber</a>
 </div>
--- templates/tribo/modules/topMenu.view.php
+++ templates/tribo/modules/topMenu.view.php
@@ -16,7 +16,7 @@
 						<div class="forgot col-md-4"><button type="submit" class="btn btn-tribo-grey ladda-button">Registrate</button></div>
 						<div style="clear: both;"></div>
 						<br />
-						<form class="l_form" action="http://interacis.com/login" method="POST"> 
+						<form class="l_form" action="" method="POST"> 
 							<div class="col-md-12"><input class="form-control" type="text" name="user" placeholder="Usuario" value="" /></div>
 							<div class="col-md-12"><input class="form-control" type="password" name="passw" placeholder="Password"></div>
 							<div class="forgot col-md-8"><a href="#">¿has olvidado tu contraseña?</a></div>
@@ -26,12 +26,49 @@
 					</div>
 
 
-					<a class='rsep' href='#'>
+					<a class='rsep search' href='#'>
 						<img src='<?=Url::template("/img/lupa.png");?>' title='Buscar' />
 					</a>
-					<a class='rsep' href='#'>
+					<div class="search_form" style="display: none;">
+						<div class="forgot col-md-12"><img style="float: left;" src='<?=Url::template("/img/lupa.png");?>' title='Login' /><h1>&nbsp;&nbsp;&nbsp;BUSCADOR DE PROGRAMAS</h1></div>
+						<div style="clear: both;"></div>
+						<br />
+						<form class="l_form" action="" method="POST"> 
+							<div class="col-md-12"><input class="form-control" type="text" name="name" placeholder="Nombre" value="" /></div>
+							<div class="forgot col-md-8"></div>
+							<div class="col-md-4 l-right"><button type="submit" class="btn btn-tribo-blue ladda-button">Buscar</button></div>
+							<div style="clear: both;"></div>
+						</form>
+					</div>
+					<a class='rsep contact' href='#'>
 						<img src='<?=Url::template("/img/contact.png");?>' title='Contacta' />
 					</a>
+					<div class="contact_form" style="display: none;">
+						<div class="forgot col-md-12"><img style="float: left;" src='<?=Url::template("/img/contact.png");?>' title='Login' /><h1>&nbsp;&nbsp;&nbsp;CONTACTA CON NOSOTROS</h1></div>
+						<div style="clear: both;"></div>
+						<br />
+						<form class="l_form" action="" method="POST"> 
+							<div class="col-md-12"><input class="form-control" type="text" name="name" placeholder="Nombre" value="" /></div>
+							<div class="col-md-12"><input class="form-control" type="text" name="email" placeholder="Email"></div>
+
+							<div class="col-md-12">
+								<select class="form-control" name="withwho">
+									<option value="all">Con quien quieres contactar</option>
+									<?php
+									for($x=0; $x<10; $x++){
+										?><option value="<?= ($x+1); ?>">Persona <?= ($x+1); ?></option><?php
+									}
+									?>
+								</select>
+							</div>
+							<div class="col-md-12">
+								<textarea class="form-control" name="mensaje" placeholder="Mensaje"></textarea>
+							</div>
+							<div class="forgot col-md-8"></div>
+							<div class="col-md-4 l-right"><button type="submit" class="btn btn-tribo-blue ladda-button">Enviar</button></div>
+							<div style="clear: both;"></div>
+						</form>
+					</div>
 				</div>
 			</div>
 			<div class='col-md-3  col-md-offset-1'>
@@ -55,9 +92,15 @@
    		$(document).on("click",".login",function(){
         	$('.login_form').add('.mask').fadeIn();      
         });
+        $(document).on("click",".contact",function(){
+        	$('.contact_form').add('.mask').fadeIn();      
+        });
+        $(document).on("click",".search",function(){
+        	$('.search_form').add('.mask').fadeIn();      
+        });
         $(document).on("click",".mask",function(){
         	//$('.login_form').add('.mask').fadeOut();
-        	$('.feedback_popup').add('.login_form').add('.mask').fadeOut();
+        	$('.login_form').add('.contact_form').add('.search_form').add('.mask').fadeOut();
         });
     </script>
     <!-- /.container -->
