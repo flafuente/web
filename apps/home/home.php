c02ffd3213f415161bd501504d6d5d78e8c52a5c
gerard.arall@gmail.com <gerard.arall@gmail.com>
2014-07-05 14:10:39 +0200

    Fix home

--- apps/home/home.php
+++ apps/home/home.php
@@ -2,26 +2,19 @@
 //No direct access
 defined('_EXE') or die('Restricted access');
 
-class homeController extends Controller {
-
-	public function init(){}
+class homeController extends Controller
+{
+    public function init() {}
 
 /*
-	public function index(){
-		$html = $this->view("views.secciones");
-		$this->render($html);
-	}
+    public function index()
+    {
+        $html = $this->view("views.secciones");
+        $this->render($html);
+    }
 */
-	public function index(){
-		$user = Registry::getUser();
-		if($user->id){
-			Helper::redirect(Url::site("home/home"));
-		}else{
-			$this->render($html, "landing");
-		}
-	}
-
-	public function home(){
-		$this->render();
-	}
+    public function index()
+    {
+        $this->render($html, "landing");
+    }
 }
