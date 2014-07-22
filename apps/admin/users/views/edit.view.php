b963bcce301d779866843ef008fdfbe30a07840c
gerard.arall@gmail.com <gerard.arall@gmail.com>
2014-07-22 19:37:37 +0200

    Admin Usuarios - cambios #45

--- apps/admin/users/views/edit.view.php
+++ apps/admin/users/views/edit.view.php
@@ -57,154 +57,174 @@ Toolbar::render();
     <input type="hidden" name="id" value="<?=$user->id?>">
     <div class="row">
         <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    Detalles
-                </div>
-                <div class="panel-body">
-                    <!-- Estado -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">
-                            Estado
-                        </label>
-                        <div class="col-sm-10">
-                            <input type="hidden" name="statusId" value="0">
-                            <input type="checkbox" class="switch" name="statusId" id="statusId" value="1" <?php if($user->statusId) echo "checked";?>>
-                        </div>
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Cuenta
                     </div>
-                    <!-- Rol -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">
-                            Rol
-                        </label>
-                        <div class="col-sm-8">
-                            <?=HTML::select("roleId", $user->roles, $user->roleId, array("id" => "roleId")); ?>
-                        </div>
-                    </div>
-                    <!-- Accesos -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">
-                            Accesos
-                        </label>
-                        <div class="col-sm-8">
-                            <?php $permisos = $user->getCategoriasIds(); ?>
-                            <?=HTML::select("permisos[]", $user->secciones, $permisos, array("class" => "select2", "multiple" => "true")); ?>
-                        </div>
-                    </div>
-                    <!-- Categorías -->
-                    <?php if (is_array($categorias) && count($categorias)) { ?>
-                        <div class="form-group" id="fieldCategoria" style="display:none">
-                            <label class="col-sm-2 control-label">
-                                Categoría
+                    <div class="panel-body">
+                        <!-- Estado -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Estado
                             </label>
                             <div class="col-sm-8">
-                                <?php $categoriasUser = $user->getCategoriasIds(); ?>
-                                <?=HTML::select("categorias[]", $categorias, $categoriasUser, array("class" => "select2", "multiple" => "true"), null, array("display" => "nombre")); ?>
+                                <input type="hidden" name="statusId" value="0">
+                                <input type="checkbox" class="switch" name="statusId" id="statusId" value="1" <?php if($user->statusId) echo "checked";?>>
                             </div>
                         </div>
-                    <?php } ?>
-                    <!-- Nombre -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">
-                            Nombre
-                        </label>
-                        <div class="col-sm-8">
-                            <input type="text" id="nombre" name="nombre" class="form-control" value="<?=Helper::sanitize($user->nombre);?>">
+                        <!-- Rol -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Rol
+                            </label>
+                            <div class="col-sm-8">
+                                <?=HTML::select("roleId", $user->roles, $user->roleId, array("id" => "roleId")); ?>
+                            </div>
+                        </div>
+                        <!-- Accesos -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Accesos
+                            </label>
+                            <div class="col-sm-8">
+                                <?php $permisos = $user->getCategoriasIds(); ?>
+                                <?=HTML::select("permisos[]", $user->secciones, $permisos, array("class" => "select2", "multiple" => "true")); ?>
+                            </div>
+                        </div>
+                        <!-- Categorías -->
+                        <?php if (is_array($categorias) && count($categorias)) { ?>
+                            <div class="form-group" id="fieldCategoria" style="display:none">
+                                <label class="col-sm-3 control-label">
+                                    Categoría
+                                </label>
+                                <div class="col-sm-8">
+                                    <?php $categoriasUser = $user->getCategoriasIds(); ?>
+                                    <?=HTML::select("categorias[]", $categorias, $categoriasUser, array("class" => "select2", "multiple" => "true"), null, array("display" => "nombre")); ?>
+                                </div>
+                            </div>
+                        <?php } ?>
+                        <!-- Email -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Email
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="text" id="email" name="email" class="form-control" value="<?=Helper::sanitize($user->email);?>">
+                            </div>
+                        </div>
+                        <!-- Contraseña -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Contraseña
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="password" id="password" name="password" class="form-control">
+                            </div>
+                        </div>
+                        <!-- Username -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Alias
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="text" id="username" name="username" class="form-control" value="<?=Helper::sanitize($user->username);?>">
+                            </div>
                         </div>
                     </div>
-                    <!-- Apellidos -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">
-                            Apellidos
-                        </label>
-                        <div class="col-sm-8">
-                            <input type="text" id="apellidos" name="apellidos" class="form-control" value="<?=Helper::sanitize($user->apellidos);?>">
-                        </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Datos personales
                     </div>
-                    <!-- Email -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">
-                            Email
-                        </label>
-                        <div class="col-sm-8">
-                            <input type="text" id="email" name="email" class="form-control" value="<?=Helper::sanitize($user->email);?>">
+                    <div class="panel-body">
+                        <!-- Foto -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Foto
+                            </label>
+                            <div class="col-sm-3">
+                                <img src="<?=$user->getFotoUrl();?>" width="100%">
+                            </div>
                         </div>
-                    </div>
-                    <!-- Contraseña -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">
-                            Contraseña
-                        </label>
-                        <div class="col-sm-8">
-                            <input type="password" id="password" name="password" class="form-control">
+                        <!-- Nombre -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Nombre
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="text" id="nombre" name="nombre" class="form-control" value="<?=Helper::sanitize($user->nombre);?>">
+                            </div>
                         </div>
-                    </div>
-                    <!-- Username -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">
-                            Alias
-                        </label>
-                        <div class="col-sm-10">
-                            <input type="text" id="username" name="username" class="form-control" value="<?=Helper::sanitize($user->username);?>">
+                        <!-- Apellidos -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Apellidos
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="text" id="apellidos" name="apellidos" class="form-control" value="<?=Helper::sanitize($user->apellidos);?>">
+                            </div>
                         </div>
-                    </div>
-                    <!-- Sexo -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">
-                            Sexo
-                        </label>
-                        <div class="col-sm-10">
-                            <?php $s = array();?>
-                            <?php $s[$user->sexo]; ?>
-                            <input type="radio" goup="sexo" name="sexo" id="sexo1" value="1" <?=$s[1]?>>
-                            Femenino
-                            <input type="radio" goup="sexo" name="sexo" id="sexo2" value="2" <?=$s[1]?>>
-                            Masculino
+                        <!-- Sexo -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Sexo
+                            </label>
+                            <div class="col-sm-8">
+                                <?php $s = array();?>
+                                <?php $s[$user->sexo]; ?>
+                                <input type="radio" goup="sexo" name="sexo" id="sexo1" value="1" <?=$s[1]?>>
+                                Femenino
+                                <input type="radio" goup="sexo" name="sexo" id="sexo2" value="2" <?=$s[1]?>>
+                                Masculino
+                            </div>
                         </div>
-                    </div>
-                    <!-- Cumpleaños -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">
-                            Cumpleaños
-                        </label>
-                        <div class="col-sm-10">
-                            <input type="date" id="fechaNacimiento" name="fechaNacimiento" class="form-control" value="<?=date("m/d/Y", strtotime($user->fechaNacimiento));?>">
+                        <!-- Cumpleaños -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Cumpleaños
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="date" id="fechaNacimiento" name="fechaNacimiento" class="form-control" value="<?=date("m/d/Y", strtotime($user->fechaNacimiento));?>">
+                            </div>
                         </div>
-                    </div>
-                    <!-- Ubicacion -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">
-                            Ubicacion
-                        </label>
-                        <div class="col-sm-10">
-                            <input type="text" id="ubicacion" name="ubicacion" class="form-control" value="<?=Helper::sanitize($user->ubicacion);?>">
+                        <!-- Ubicacion -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Ubicacion
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="text" id="ubicacion" name="ubicacion" class="form-control" value="<?=Helper::sanitize($user->ubicacion);?>">
+                            </div>
                         </div>
-                    </div>
-                    <!-- Biografía -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">
-                            Biografía
-                        </label>
-                        <div class="col-sm-10">
-                            <textarea id="biografia" name="biografia" class="form-control"><?=Helper::sanitize($user->biografia);?></textarea>
+                        <!-- Biografía -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Biografía
+                            </label>
+                            <div class="col-sm-8">
+                                <textarea id="biografia" name="biografia" class="form-control"><?=Helper::sanitize($user->biografia);?></textarea>
+                            </div>
                         </div>
-                    </div>
-                    <!-- Teléfono -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">
-                            Teléfono
-                        </label>
-                        <div class="col-sm-10">
-                            <input type="text" id="telefono" name="telefono" class="form-control" value="<?=Helper::sanitize($user->telefono);?>">
+                        <!-- Teléfono -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Teléfono
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="text" id="telefono" name="telefono" class="form-control" value="<?=Helper::sanitize($user->telefono);?>">
+                            </div>
                         </div>
-                    </div>
-                    <!-- Sitios -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">
-                            Sitios
-                        </label>
-                        <div class="col-sm-10">
-                            <input type="text" id="sitios" name="sitios" class="form-control" value="<?=Helper::sanitize($user->sitios);?>">
+                        <!-- Sitios -->
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">
+                                Sitios
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="text" id="sitios" name="sitios" class="form-control" value="<?=Helper::sanitize($user->sitios);?>">
+                            </div>
                         </div>
                     </div>
                 </div>
