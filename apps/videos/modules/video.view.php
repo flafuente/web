5b93ee8bf2c1b68c90d73df9e82787d4314e2dcc
gerard.arall@gmail.com <gerard.arall@gmail.com>
2014-08-24 09:44:41 +0200

    Mis videos: Btn

--- apps/videos/modules/video.view.php
+++ apps/videos/modules/video.view.php
@@ -2,23 +2,23 @@
 
 <div class="mi-video">
     <div class="col-md-6 mi-video-prev">
-    	<img src="<?=Url::template("img/programas/malviviendo.jpg");?>" />
+        <img src="<?=Url::template("img/programas/malviviendo.jpg");?>" />
     </div>
     <div class="col-md-6" style="padding-top: 20px;">
         <span class="mivid-titulo">
-        	<?=Helper::sanitize($video->titulo)?>
+            <?=Helper::sanitize($video->titulo)?>
         </span>
         <br />
         <span class="mivid-subtitulo">
-        	Los vídeos no tienen subtítulos
+            Los vídeos no tienen subtítulos
         </span>
         <br />
         <span class="mv-fecha">
-        	<?=date("d/m/Y", strtotime($video->dateInsert));?> a las <?=date("H:i", strtotime($video->dateInsert));?>
+            <?=date("d/m/Y", strtotime($video->dateInsert));?> a las <?=date("H:i", strtotime($video->dateInsert));?>
         </span>
-        <span class="btn btn-tribo-grey" urledit="<?=Url::site("videos/ver/".$video->id);?>" style="float: right;">
-        	Editar
-        </span>
+        <a class="btn-tribo-grey btn ladda-button" href="<?=Url::site("videos/ver/".$video->id);?>" style="float: right;">
+            Editar
+        </a>
     </div>
 </div>
 <div style="clear: both;"></div>
