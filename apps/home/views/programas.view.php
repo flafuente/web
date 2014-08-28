f1c92c8049990b7e4553619866363f9ce8ad318b
gerard.arall@gmail.com <gerard.arall@gmail.com>
2014-08-28 19:01:12 +0200

    Home: warning fix

--- apps/home/views/programas.view.php
+++ apps/home/views/programas.view.php
@@ -1,22 +1,24 @@
 <?php defined('_EXE') or die('Restricted access'); ?>
 
-<?php foreach ($programas as $programa) { ?>
-    <div class='col-md-6 square'>
-        <a href="<?=Url::site("/programas/ver/".$programa->slug);?>">
-            <img src="<?=$programa->getThumbnailUrl()?>" title="<?=$programa->titulo;?>" />
-        </a>
-         <img class="arrow" src="<?=Url::template("img/arrow.png")?>" />
-        <div class="rating"><?=HTML::showRate(rand(10, 999), 999, 10);?></div>
-        <div class="sq_content">
-            <div class="sq_title">
-                <a href="<?=Url::site("/programas/ver/".$programa->slug);?>">
-                    <?=$programa->titulo;?>
-                </a>
-            </div>
-            <div class="sq_num">
-                <?=rand(10, 999); ?>
-                <i class="fa fa-heart-o"></i>
+<?php if (count($programas)) { ?>
+    <?php foreach ($programas as $programa) { ?>
+        <div class='col-md-6 square'>
+            <a href="<?=Url::site("/programas/ver/".$programa->slug);?>">
+                <img src="<?=$programa->getThumbnailUrl()?>" title="<?=$programa->titulo;?>" />
+            </a>
+             <img class="arrow" src="<?=Url::template("img/arrow.png")?>" />
+            <div class="rating"><?=HTML::showRate(rand(10, 999), 999, 10);?></div>
+            <div class="sq_content">
+                <div class="sq_title">
+                    <a href="<?=Url::site("/programas/ver/".$programa->slug);?>">
+                        <?=$programa->titulo;?>
+                    </a>
+                </div>
+                <div class="sq_num">
+                    <?=rand(10, 999); ?>
+                    <i class="fa fa-heart-o"></i>
+                </div>
             </div>
         </div>
-    </div>
+    <?php } ?>
 <?php } ?>
