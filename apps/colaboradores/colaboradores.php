9b105df8e1cc9bb18a1228998247b79e3d8f4b0e
gerard.arall@gmail.com <gerard.arall@gmail.com>
2014-09-02 21:03:01 +0200

    Fixing... #82

--- apps/colaboradores/colaboradores.php
+++ apps/colaboradores/colaboradores.php
@@ -8,7 +8,7 @@ class colaboradoresController extends Controller
 
     public function index()
     {
-        $this->setData("categorias", Categoria::select(array("tipoId" => 2)));
+        $this->setData("categorias", Categoria::select(array("tipoId" => 1)));
         $html = $this->view("views.colaboradores");
         $this->render($html);
     }
