831e4525e72692f4be98a3368ff2eaf9a7f213d2
gerard.arall@gmail.com <gerard.arall@gmail.com>
2014-09-08 22:39:34 +0200

    HTML::wistia quienessomos #89

--- apps/quienessomos/views/quienessomos.view.php
+++ apps/quienessomos/views/quienessomos.view.php
@@ -1,33 +1,35 @@
-<?php defined('_EXE') or die('Restricted access');
+<?php defined('_EXE') or die('Restricted access'); ?>
 
-?>
 <div class='col-md-12 serie_info'>
+
     <div class="col-md-12 video">
-		<div id="wistia_2uu6gseu6n" class="wistia_embed" style="width:558px;height:314px;"> </div>
-		<script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/E-v1.js"></script>
-		<script>
-		wistiaEmbed = Wistia.embed("2uu6gseu6n");
-		</script>	
+        <?=HTML::wistiaPlayer("2uu6gseu6n", "558", "314");?>
     </div>
+
     <div class="video-info">
+
         <div class="col-md-12">
             <div class="vd-codigo" style="text-transform:uppercase; padding-top: 20px;">Somos Tribo, un canal de televisión hecho por personas para personas</div>
         </div>
+
         <div class="col-md-12 video-desc">
-			Tribo somos un grupo de personas dispuestas a arriesgarnos y saltarnos las reglas televisivas.
-			<br />
-			<br />
-			Emprendedores, jóvenes, hablamos el mismo idioma que tú, que internet, porque somos gente de la calle, y hemos decidido reunirnos para hacerte la vida más fácil.
-			<br />
-			<br />
-			A nosotros nos encanta navegar por Internet, pero nuestro pequeño hándicap es que nos gustan las cosas ordenadas y cómodas. Por eso vamos a seleccionar cuidadosamente 
-			todo el contenido de Internet que esté reventando las visitas, los contenidos <b>MÁS VISTOS</b> y <b>MÁS INFLUYENTES</b>, los mismos contenidos que buscas cada vez que te metes en 
-			tu navegador, y te lo ponemos en bandeja en nuestro canal de televisión, con nuevos contenidos creados por ellos mismos, y ¡sin que muevas un dedo!
-			<br />
-			<br />
-			Somos una cadena de personas donde vosotros decidís, hacéis y veis. Sólo las personas saben lo que les gusta a las personas.
-			<br /><br />
+            Tribo somos un grupo de personas dispuestas a arriesgarnos y saltarnos las reglas televisivas.
+            <br />
+            <br />
+            Emprendedores, jóvenes, hablamos el mismo idioma que tú, que internet, porque somos gente de la calle, y hemos decidido reunirnos para hacerte la vida más fácil.
+            <br />
+            <br />
+            A nosotros nos encanta navegar por Internet, pero nuestro pequeño hándicap es que nos gustan las cosas ordenadas y cómodas. Por eso vamos a seleccionar cuidadosamente
+            todo el contenido de Internet que esté reventando las visitas, los contenidos <b>MÁS VISTOS</b> y <b>MÁS INFLUYENTES</b>, los mismos contenidos que buscas cada vez que te metes en
+            tu navegador, y te lo ponemos en bandeja en nuestro canal de televisión, con nuevos contenidos creados por ellos mismos, y ¡sin que muevas un dedo!
+            <br />
+            <br />
+            Somos una cadena de personas donde vosotros decidís, hacéis y veis. Sólo las personas saben lo que les gusta a las personas.
+            <br /><br />
         </div>
+
         <div style="clear: both;"></div>
+
     </div>
+
 </div>
