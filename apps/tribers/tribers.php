394742a29c63ddc778b7ab58ca035c3cb55a89a8
gerard.arall@gmail.com <gerard.arall@gmail.com>
2014-09-08 17:59:38 +0200

    Tribers

--- apps/tribers/tribers.php
+++ apps/tribers/tribers.php
@@ -4,13 +4,22 @@ defined('_EXE') or die('Restricted access');
 
 class tribersController extends Controller
 {
-    public function init() {
-    	$triber = Registry::getTriber();
-    }
+    public function init() {}
 
-    public function index(){
-        $html = $this->view("views.tribers");
-        $this->render($html);
+    public function index() {}
+
+    public function ver()
+    {
+        $url = Registry::getUrl();
+
+        $user = new User($url->vars[0]);
+        if ($user->id) {
+            $this->setData("triber", $user);
+            $html = $this->view("views.triber");
+            $this->render($html);
+        } else {
+            Url::redirect(Url::site());
+        }
     }
 
 }
