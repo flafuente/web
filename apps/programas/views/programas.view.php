f7ea185caadefa9ec2f46d61cf84f76e041c2873
gerard.arall@gmail.com <gerard.arall@gmail.com>
2014-09-16 12:16:04 +0200

    Programas sin subtitulo #149

--- apps/programas/views/programas.view.php
+++ apps/programas/views/programas.view.php
@@ -9,26 +9,37 @@
 <div class="programas">
     <?php if (count($programas)) { ?>
         <?php foreach ($programas as $i=>$programa) { ?>
+
             <?php if ($i>=$limit) { ?>
                 <?php $class = "hidden"; ?>
             <?php } ?>
+
+            <!-- Programa -->
             <a href="<?=Url::site("programas/ver/".$programa->slug);?>" class="<?=$class;?>">
                 <div class='col-md-6 square'>
+                    <!-- Imágen -->
                     <img src="<?=$programa->getThumbnailUrl()?>" title="<?=$programa->titulo; ?>" />
-                    <img class="arrow" src="<?=Url::template("img/arrow.png")?>" />
-                    <div class="sq_content">
-                        <div class="sq_title">
-                            <?=$programa->subtitulo; ?>
+                    <!-- Subtítulo -->
+                    <?php if ($programa->subtitulo) { ?>
+                        <img class="arrow" src="<?=Url::template("img/arrow.png")?>" />
+                        <div class="sq_content">
+                            <div class="sq_title">
+                                <?=$programa->subtitulo; ?>
+                            </div>
                         </div>
-                    </div>
+                    <?php } ?>
                 </div>
             </a>
+
         <?php } ?>
+
+        <!-- Paginación -->
         <?php if ($i>=$limit) { ?>
             <div class="col-md-offset-6 col-md-6 ver-todas-web">
                 Ver más&nbsp;&nbsp;<div class="circulo-azul more">+</div>
             </div>
         <?php } ?>
+
     <?php } ?>
 </div>
 
