038b58403f97fb2802a1b4c296ada8be5f1fa677
gerard.arall@gmail.com <gerard.arall@gmail.com>
2014-10-02 13:38:35 +0200

    Sintonizanos

--- apps/sintonizanos/views/sintonizanos.view.php
+++ apps/sintonizanos/views/sintonizanos.view.php
@@ -8,28 +8,27 @@
             ¿DÓNDE PUEDES VER TRIBO?
         </div>
         <div class="canalesd">
-			<div class='title-linewh'>
-				<span><img src='<?=Url::template("img/sintonizanos/TDT2.png")?>' title='tdt'></span>
-			</div>
-			<p class='center'>Actualmente, Tribo TV mantiene una emisión en pruebas en la Comunidad de Madrid.
-			<br />Puedes sintonizarnos en las siguientes frecuencias:</p>
-			<div class='region col-md-6'>
-				<p><span class='blue'>MADRID</span> - Canal 39 - 618 MHz</p>
-				<p><span class='blue'>ARANJUEZ</span> - Canal 21 - 474 MHz</p>
-				<p><span class='blue'>ALCOBENDAS</span> - Canal 51 - 714 MHz</p>
-				<p><span class='blue'>MOSTOLES</span> - Canal 39 - 618 MHz</p>
-				<p><span class='blue'>SOTO DEL REAL</span> - Canal 35 - 586 MHz</p>
-			</div>
-			<div class='region col-md-6'>
-				<p><span class='blue'>Fuenlabrada</span> - Canal 42 - 642 MHz</p>
-				<p><span class='blue'>COLLADO VILLALBA</span> - Canal 29 - 538 MHz</p>
-				<p><span class='blue'>ALCALA DE HENARES</span> - Canal 46 - 674 MHz</p>
-				<p><span class='blue'>POZUELO</span> - Canal 23 - 490 MHz</p>
-				<p><span class='blue'>SAN MARTIN DE VALDEIGLESIAS</span> - Canal 51 - 714 MHz</p>
-			</div>
-			<div style='clear:both;'></div>
-			<div class='title-linewh'></div>
-			<p class='center blue'>Y próximamente, Tribo TV en más Comunidades Autónomas</p>
+            <div class='title-linewh'>
+                <span><img src='<?=Url::template("img/sintonizanos/TDT2.png")?>' title='tdt'></span>
+            </div>
+            <p class='center'>Actualmente, puedes sintonizarnos en las siguientes ciudades:</p>
+            <div class='region col-md-6'>
+                <p><span class='blue'>MADRID</span> - Canal 39 - 618 MHz</p>
+                <p><span class='blue'>ARANJUEZ</span> - Canal 21 - 474 MHz</p>
+                <p><span class='blue'>ALCOBENDAS</span> - Canal 51 - 714 MHz</p>
+                <p><span class='blue'>MOSTOLES</span> - Canal 39 - 618 MHz</p>
+                <p><span class='blue'>SOTO DEL REAL</span> - Canal 35 - 586 MHz</p>
+            </div>
+            <div class='region col-md-6'>
+                <p><span class='blue'>Fuenlabrada</span> - Canal 42 - 642 MHz</p>
+                <p><span class='blue'>COLLADO VILLALBA</span> - Canal 29 - 538 MHz</p>
+                <p><span class='blue'>ALCALA DE HENARES</span> - Canal 46 - 674 MHz</p>
+                <p><span class='blue'>POZUELO</span> - Canal 23 - 490 MHz</p>
+                <p><span class='blue'>SAN MARTIN DE VALDEIGLESIAS</span> - Canal 51 - 714 MHz</p>
+            </div>
+            <div style='clear:both;'></div>
+            <div class='title-linewh'></div>
+            <p class='center blue'>Y próximamente, Tribo TV en más Comunidades Autónomas</p>
 
         </div>
        <div style="clear: both;"></div>
