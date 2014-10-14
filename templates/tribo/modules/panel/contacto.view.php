fe325b53bbfe7dff077eaf9e11fef16eed532804
gerard.arall@gmail.com <gerard.arall@gmail.com>
2014-10-14 12:29:00 +0200

    Traducciones #189

--- templates/tribo/modules/panel/contacto.view.php
+++ templates/tribo/modules/panel/contacto.view.php
@@ -6,20 +6,20 @@ if (Registry::getUser()->id) {
 }
 ?>
 <a class='rsep contact' href='#'>
-    <img src='<?=Url::template("/img/contact.png");?>' title='Contacta' />
+    <img src='<?=Url::template("/img/contact.png");?>' title='<?=Language::translate("VIEW_TEMPLATE_CONTACTO_CONTACTA");?>' />
 </a>
 <div class="contact_form" style="display: none;<?=$stl;?>">
-    <div class="forgot col-md-12"><img class="imgmdl" src='<?=Url::template("/img/contact.png");?>' title='Login' /><h1>&nbsp;&nbsp;&nbsp;CONTACTA CON NOSOTROS</h1></div>
+    <div class="forgot col-md-12"><img class="imgmdl" src='<?=Url::template("/img/contact.png");?>' title='Login' /><h1>&nbsp;&nbsp;&nbsp;<?=Language::translate("VIEW_TEMPLATE_CONTACTO_TITLE");?></h1></div>
     <div style="clear: both;"></div>
     <br />
     <form class="l_form ajax" role="form" method="post" name="mainForm" id="mainForm" action="<?=Url::site("contacto/enviar");?>">
         <!-- Nombre -->
         <div class="col-md-12">
-            <input class="form-control" type="text" name="nombre" placeholder="Nombre" value="" />
+            <input class="form-control" type="text" name="nombre" placeholder="<?=Language::translate("VIEW_TEMPLATE_CONTACTO_NOMBRE");?>" value="" />
         </div>
         <!-- Email -->
         <div class="col-md-12">
-            <input class="form-control" type="text" name="email" placeholder="Email">
+            <input class="form-control" type="text" name="email" placeholder="<?=Language::translate("VIEW_TEMPLATE_CONTACTO_EMAIL");?>">
         </div>
         <?php $contactos = Contacto::select(array("visible" => true, "order" => "nombre")); ?>
         <?php if (count($contactos)) { ?>
@@ -30,12 +30,12 @@ if (Registry::getUser()->id) {
         <?php } ?>
         <!-- Mensaje -->
         <div class="col-md-12">
-            <textarea class="form-control" name="mensaje" placeholder="Mensaje"></textarea>
+            <textarea class="form-control" name="mensaje" placeholder="<?=Language::translate("VIEW_TEMPLATE_CONTACTO_MENSAJE");?>"></textarea>
         </div>
         <div class="forgot col-md-8"></div>
         <!-- Buttons -->
         <div class="col-md-4 l-right">
-            <?=HTML::formButton("btn-tribo-blue", null, "Enviar", array(
+            <?=HTML::formButton("btn-tribo-blue", null, Language::translate("BTN_SEND"), array(
                     "data-app" => "contacto",
                     "data-action" => "enviar"
                 )
--- templates/tribo/modules/panel/search.view.php
+++ templates/tribo/modules/panel/search.view.php
@@ -6,19 +6,19 @@ if (Registry::getUser()->id) {
 }
 ?>
 <a class='rsep search' href='#'>
-    <img src='<?=Url::template("/img/lupa.png");?>' title='Buscar' />
+    <img src='<?=Url::template("/img/lupa.png");?>' title='<?=Language::translate("VIEW_TEMPLATE_SEARCH_BUSCAR");?>' />
 </a>
 <div class="search_form" style="display: none;<?=$stl;?>">
     <div class="forgot col-md-12">
         <img class="imgmdl" src='<?=Url::template("/img/lupa.png");?>' title='Login' />
-        <h1>&nbsp;&nbsp;&nbsp;BUSCADOR DE PROGRAMAS</h1>
+        <h1>&nbsp;&nbsp;&nbsp;<?=Language::translate("VIEW_TEMPLATE_SEARCH_TITLE");?></h1>
     </div>
     <div style="clear: both;"></div>
     <br />
     <form class="l_form" action="" method="POST">
-        <div class="col-md-12"><input class="form-control" type="text" name="search" id="search" placeholder="Buscar..."/></div>
+        <div class="col-md-12"><input class="form-control" type="text" name="search" id="search" placeholder="<?=Language::translate("VIEW_TEMPLATE_SEARCH_BUSCAR");?>..."/></div>
         <div class="forgot col-md-8"></div>
-        <!--<div class="col-md-4 l-right"><button type="submit" class="btn btn-tribo-blue ladda-button">Buscar</button></div>-->
+        <!--<div class="col-md-4 l-right"><button type="submit" class="btn btn-tribo-blue ladda-button"><?=Language::translate("VIEW_TEMPLATE_SEARCH_BUSCAR");?></button></div>-->
         <div style="clear: both;"></div>
         <div id="searchResults"></div>
     </form>
