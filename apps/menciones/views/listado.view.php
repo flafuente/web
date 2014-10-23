bcf7722718a670a7243da5bbcf05d9dccc82eda8
gerard.arall@gmail.com <gerard.arall@gmail.com>
2014-10-23 20:24:14 +0200

    menciones #206

--- apps/menciones/views/listado.view.php
+++ apps/menciones/views/listado.view.php
@@ -1,23 +1,25 @@
 <?php defined('_EXE') or die('Restricted access'); ?>
 
-<div class='col-md-12'>
-    <div class="square-info-foro">
-        <div class="grey" style="margin-bottom: 0px;">
-            <?=Language::translate("VIEW_MENCIONES_LISTADO_TITLE");?>
+<form class="form-horizontal" role="form" method="post" action="<?=Url::site("menciones");?>">
+    <div class='col-md-12'>
+        <div class="square-info-foro">
+            <div class="grey" style="margin-bottom: 0px;">
+                <?=Language::translate("VIEW_MENCIONES_LISTADO_TITLE");?>
+            </div>
         </div>
-    </div>
 
-    <?php
-    if (count($menciones)) {
-        foreach ($menciones as $mencion) {
-            $controller->setData("mencion", $mencion);
-            echo $controller->view("modules.mencion-mini");
-        }
-        $controller->setData("pag", $pag);
-        echo $controller->view("modules.pagination");
-    } else { ?>
-        <blockquote>
-            <p> <?=Language::translate("VIEW_MENCIONES_LISTADO_EMPTY");?> </p>
-        </blockquote>
-    <?php } ?>
-</div>
+        <?php
+        if (count($menciones)) {
+            foreach ($menciones as $mencion) {
+                $controller->setData("mencion", $mencion);
+                echo $controller->view("modules.mencion-mini");
+            }
+            $controller->setData("pag", $pag);
+            echo $controller->view("modules.pagination");
+        } else { ?>
+            <blockquote>
+                <p> <?=Language::translate("VIEW_MENCIONES_LISTADO_EMPTY");?> </p>
+            </blockquote>
+        <?php } ?>
+    </div>
+</form>
